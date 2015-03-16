c6016a1d8519aec023b6d26b1c60a619e307c167
Kevin Foley <kfoley@squarespace.com>
2015-03-16 19:57:31 -0400

    Adding a readme, preserving comments, and updating the build script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglifyjs');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var jsdoc  = require('gulp-jsdoc-to-markdown');
+var rename = require('gulp-rename');
 
 gulp.task('lint', function() {
   return gulp.src('./src/**/*.js')
@@ -8,15 +11,23 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter('default'));
 });
 
+gulp.task('document', function() {
+  return gulp.src('./src/rafscroll.js')
+    .pipe(jsdoc())
+    .pipe(rename('README.md'))
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('build-polyfill', ['lint'], function() {
   return gulp.src(['./src/lib/rAF.js', './src/rafscroll.js'])
-    .pipe(uglify('rafscroll.polyfill.min.js'))
+    .pipe(concat('rafscroll.polyfill.min.js'))
+    .pipe(uglify({ preserveComments: 'some' }))
     .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build', ['lint'], function() {
   return gulp.src('./src/rafscroll.js')
-    .pipe(uglify('rafscroll.min.js'))
+    .pipe(uglify({ preserveComments: 'some' }))
     .pipe(gulp.dest('./dist'));
 });
 
